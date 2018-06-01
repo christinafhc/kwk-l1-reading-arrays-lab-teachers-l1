c3b79a7f83a417cb5678c3f12fdbfccb7051a7d6
Chris Chen <christinafh.chen@gmail.com>
2018-06-01 19:10:17 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -34,6 +34,7 @@ end
 def last_student_by_method
   # Write a solution that returns the first student using the built in .last method
   puts STUDENT_NAMES.last
+end
 
 def first_second_and_third_students
   # Write a solution that returns the first, second and third students
