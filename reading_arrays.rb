2cca06c6708dd697a9ece776d207a13358d99f90
Chris Chen <christinafh.chen@gmail.com>
2018-06-01 18:58:36 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -9,7 +9,7 @@ student_names = [
 
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
-  puts student_names.first
+  puts student_names[0]
 end
 
 def fourth_student_by_index
@@ -18,10 +18,12 @@ end
 
 def last_student_by_index
   # Write a solution that returns the first student in the array student_names
+  puts student_names.last 
 end
 
 def first_student_by_method
   # Write a solution that returns the first student using the built in .first method
+  puts student_names.first
 end
 
 def last_student_by_method
