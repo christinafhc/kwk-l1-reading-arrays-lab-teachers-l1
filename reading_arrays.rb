c5e33080c8b5d613a7fa3ffcb3910cf2bb88bec3
Chris Chen <christinafh.chen@gmail.com>
2018-06-01 19:05:31 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -7,7 +7,7 @@ STUDENT_NAMES = [
   "Rihanna"
 ]
 
-puts student_names[5]
+puts STUDENT_NAMES.last
 def first_student_by_index
   # Write a solution that returns the first student in the array student_names
   puts STUDENT_NAMES[0]
